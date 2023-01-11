442cddc8a8157bc6408c370247b8db01ff2f6a80
test12356789 <121945970+test12356789@users.noreply.github.com>
2023-01-11 19:59:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+  let temp = "";
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    temp += str[i];
+  }
+  for (let i = 0; i < temp.length / 2; i++) {
+    if (temp[i] != temp[temp.length - 1 - i]) return false;
+  }
+  return true;
 
 }
 module.exports = palindrome
